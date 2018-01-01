580e10fd98092f15ab6a6b4df5728540f42ee626
DongChen <352070977@qq.com>
2018-01-02 06:37:06 +0800

    使用EBO进行绘制

--- turtorial02/HelloTriangle.cpp
+++ turtorial02/HelloTriangle.cpp
@@ -32,7 +32,7 @@ const GLchar* fragmentShaderSource = "#version 330 core\n"
     "}\n\0";
 
 
-GLFWwindow* initGL(){
+GLFWwindow* initGL(GLFWkeyfun callback){
     #pragma region 初始化环境
     if( !glfwInit() )
 	{
@@ -58,7 +58,7 @@ GLFWwindow* initGL(){
     glfwMakeContextCurrent(window);
 
     //设置回调
-    glfwSetKeyCallback(window, key_callback);
+    glfwSetKeyCallback(window, callback);
 
     //
     glewExperimental = GL_TRUE;
@@ -122,31 +122,44 @@ GLuint buildShaderProgram(){
 
 int main(){
     
-    GLFWwindow* window = initGL();
+    GLFWwindow* window = initGL(key_callback);
     if(window == nullptr)
         return -1;
 
     GLuint shaderProgram = buildShaderProgram();
 
-    // 设置顶点数据
-    GLfloat vertices[] = {
-        -0.5f, -0.5f, 0.0f, // Left  
-         0.5f, -0.5f, 0.0f, // Right 
-         0.0f,  0.5f, 0.0f  // Top   
+    float vertices[] = {
+        0.5f, 0.5f, 0.0f,   // 右上角
+        0.5f, -0.5f, 0.0f,  // 右下角
+        -0.5f, -0.5f, 0.0f, // 左下角
+        -0.5f, 0.5f, 0.0f   // 左上角
     };
+
+    unsigned int indices[] = { // 注意索引从0开始! 
+        0, 1, 3, // 第一个三角形
+        1, 2, 3  // 第二个三角形
+    };
+
     GLuint VBO, VAO;
+    GLuint EBO;
     glGenVertexArrays(1, &VAO);//生成VAO
     glGenBuffers(1, &VBO);//生成VBO
+    glGenBuffers(1, &EBO);//生成EBO
 
     // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
-    glBindVertexArray(VAO);//绑定顶点数组
+    glBindVertexArray(VAO);//绑定顶点数组 Vertex Array Object
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);//顶点缓冲对象
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);//顶点缓冲对象 Vertex Buffer Object
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);//Element Buffer Object
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     //设置顶点属性
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
+
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);//线框模式
 
     //解绑
     // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
@@ -169,7 +182,8 @@ int main(){
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
-        glBindVertexArray(0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(NULL);
 
         // Swap the screen buffers
         glfwSwapBuffers(window);
@@ -177,6 +191,7 @@ int main(){
     // Properly de-allocate all resources once they've outlived their purpose
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     // Terminate GLFW, clearing any resources allocated by GLFW.
     glfwTerminate();
     return 0;
